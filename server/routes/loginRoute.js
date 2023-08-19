52a045d93fdf43229e938b9190700fe86a2ec10a
Clement Shum <clement.shum128@gmail.com>
2023-08-19 11:16:02 -0400

    login email and password checks

--- server/routes/loginRoute.js
+++ server/routes/loginRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 const getLoginInfo = require("../db/queries/getLoginInfo");
 
 router.post("/", (req, res) => {
@@ -7,12 +8,18 @@ router.post("/", (req, res) => {
 
   getLoginInfo(email).then((response) => {
     if (!response) {
-      return console.log("Email or password does not match please try again");
+      return console.log("Email does not exist - try again");
     }
-    if (response.email === email && response.password === password) {
-      console.log(response);
-      return res.json(response);
-    }
+    bcrypt.compare(password, response.password, function (err, result) {
+      // checks emails match and password = hash password
+      if (response.email === email && result === true) {
+        console.log("Login Successful!");
+        return res.json(response);
+      } else {
+        console.log("Password is incorrect");
+        return res.json({ password: false });
+      }
+    });
   });
 });
 
--- server/routes/signUpRoute.js
+++ server/routes/signUpRoute.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const router = express.Router();
+
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 const signUp = require("../db/queries/signUp");
 
 router.post("/", (req, res) => {
   const { email, password } = req.body;
 
-  signUp(email, password).then((response) => {
-    console.log(response);
-    return res.json(response);
+  bcrypt.hash(password, saltRounds, function (err, hash) {
+    // using bcrpy to hash password for safety
+    console.log("hashed password: ", hash);
+
+    signUp(email, hash).then((response) => {
+      console.log(response);
+      return res.json(response);
+    });
   });
 });
 
