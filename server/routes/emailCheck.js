6bf8b00845813c68e1c92fae7c82f533ba0fe39a
Clement Shum <clement.shum128@gmail.com>
2023-08-20 13:30:05 -0400

    add comments to email check

--- server/routes/emailCheck.js
+++ server/routes/emailCheck.js
@@ -9,10 +9,14 @@ router.post("/", (req, res) => {
   getAllEmails().then((results) => {
     console.log("line 10", results);
     const emails = results.map((users) => users.email);
+
+    // checks if the email user is signing up with already exists or not
+    // if it already exists
     if (emails.includes(email)) {
       console.log("Email already exist");
       return res.send({ exists: true });
     } else {
+      // if it does not exist
       console.log("Email does not exist");
       return res.send({ exists: false });
     }
