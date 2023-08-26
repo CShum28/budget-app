2d3878de1bf173cbf50aa603f5455d1d6420e406
Clement Shum <clement.shum128@gmail.com>
2023-08-26 15:15:21 -0400

    add useEffect to homepage to get user budgetlist

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,24 @@
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import Header from "../components/Header";
 import Button from "../components/Button";
+import BudgetList from "../components/BudgetList";
+import axios from "axios";
 
 function Home(props) {
-  console.log("userInfo: ", props.userInfo);
-
+  const [budgetList, setBudgetList] = useState([]);
   const nav = useNavigate();
+
+  const { id } = useParams(); // getting id to be used for GET request
+
+  useEffect(() => {
+    axios.get(`/api/get-budgets/${id}`).then((res) => {
+      // axios.get(`http://localhost:5000/api/get-budgets/${id}`).then((res) => {
+      setBudgetList(res.data);
+    });
+  }, []);
+
+  console.log("##: ", budgetList);
 
   const addNewBudget = () => {
     nav("/add-budget");
@@ -13,9 +26,11 @@ function Home(props) {
 
   return (
     <>
+      {/* This displays the user's email at the top */}
       <Header userInfo={props.userInfo} />
       <h2>This is the home page</h2>
       <Button onClick={addNewBudget}>Add New Budget!</Button>
+      <BudgetList budgetList={budgetList} />
     </>
   );
 }
