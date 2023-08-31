646177951bd9ec495615c02df69cfef4de3466e1
Clement Shum <clement.shum128@gmail.com>
2023-08-31 18:22:27 -0400

    refactor code to look cleaner

--- server/routes/updateBudgetRoute.js
+++ server/routes/updateBudgetRoute.js
@@ -4,10 +4,7 @@ const updateBudget = require("../db/queries/updateBudget");
 
 router.put("/", (req, res) => {
   const { userId, budgetId, name, budget } = req.body;
-  console.log(userId);
-  console.log(budgetId);
-  console.log(name);
-  console.log(budget);
+
   updateBudget(userId, budgetId, name, budget).then((result) => {
     res.send(result);
   });
