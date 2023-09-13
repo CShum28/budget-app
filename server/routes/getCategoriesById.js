d5032580e105016244b784f64ea86847ebfff35f
Clement Shum <clement.shum128@gmail.com>
2023-09-12 23:02:45 -0400

    remove console log

--- server/routes/getCategoriesById.js
+++ server/routes/getCategoriesById.js
@@ -4,7 +4,7 @@ const getCategories = require("../db/queries/getCategories");
 
 router.get("/:id", (req, res) => {
   const budgetId = req.params.id;
-  console.log("33", budgetId);
+
   getCategories(budgetId).then((results) => {
     res.json(results);
   });
