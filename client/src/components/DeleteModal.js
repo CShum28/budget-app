6a8d80ccf17faf620372a90ff1337ed23f70805f
Clement Shum <clement.shum128@gmail.com>
2023-09-18 18:41:56 -0400

    add classes to buttn

--- client/src/components/DeleteModal.js
+++ client/src/components/DeleteModal.js
@@ -4,10 +4,16 @@ import "../styles/DeleteModal.css";
 function DeleteModal(props) {
   return (
     <>
-      <div className="modal overlay">
+      <div className="delete modal overlay">
         <p>Are you sure you want to delete?</p>
-        <Button onClick={props.confirmDelete}>Delete</Button>
-        <Button onClick={props.toggleModal}>Cancel</Button>
+        <div>
+          <Button delete onClick={props.confirmDelete}>
+            Delete
+          </Button>
+          <Button cancel onClick={props.toggleModal}>
+            Cancel
+          </Button>
+        </div>
       </div>
     </>
   );
