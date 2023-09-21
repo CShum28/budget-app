1039dcc7dcfe158fbdc8b4af197010adf58d9a61
Clement Shum <clement.shum128@gmail.com>
2023-09-21 19:55:00 -0400

    updated checks for login and sign up

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -9,26 +9,34 @@ function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const [modal, setModal] = useState(false);
+  const [userModal, setUserModal] = useState(false); // modal to display if username already exists
+  const [emptyCheckModal, setEmptyCheckModal] = useState(false); // modal to display if input for user or password is empty
 
   const nav = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    // use emailCheck to see if email already exists or not
-    emailCheck(email).then((res) => {
-      if (res) {
-        // if email already exists
-        setModal(true);
-      } else {
-        // if email does not exist, complete sign up
-        userSignUp(email, password).then((res) => {
-          console.log("from client:", res);
-          nav("/");
-        });
-      }
-    });
+    if (!email || !password) {
+      // checks to either or them are blank
+      setUserModal(false);
+      setEmptyCheckModal(true);
+    } else {
+      // use emailCheck to see if email already exists or not
+      emailCheck(email).then((res) => {
+        if (res) {
+          // if email already exists
+          setEmptyCheckModal(false);
+          setUserModal(true);
+        } else {
+          // if email does not exist, complete sign up
+          userSignUp(email, password).then((res) => {
+            console.log("from client:", res);
+            nav("/");
+          });
+        }
+      });
+    }
   };
 
   return (
@@ -43,6 +51,7 @@ function SignUp() {
               name="email"
               type="text"
               placeholder="username"
+              maxlength="50"
               value={email}
               onChange={(e) => {
                 setEmail(e.target.value);
@@ -54,6 +63,7 @@ function SignUp() {
               name="password"
               type="password"
               placeholder="password"
+              maxlength="50"
               value={password}
               onChange={(e) => {
                 setPassword(e.target.value);
@@ -62,7 +72,12 @@ function SignUp() {
           </div>
           <Button inputSignup>Sign Up!</Button>
         </form>
-        {modal && (
+        {emptyCheckModal && (
+          <div className="signup__error">
+            Please do not leave any inputs blank
+          </div>
+        )}
+        {userModal && (
           <div className="signup__error">
             The username already exists, try again
           </div>
